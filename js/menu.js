ce30bb6fc4668fc662c3c44e38130d4ae9b72cf5
Camila Laruccia <camilalaruccia@gmail.com>
2023-04-25 15:35:27 -0300

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const botao = document.querySelector("nav h2");
+const linksMenu = document.querySelector(".menu");
+const icone = document.querySelector(".icone");
+
+botao.addEventListener("click", function (event) {
+    event.preventDefault();
+    linksMenu.classList.toggle("aberto");
+
+    /* Se a classe "aberto" estiver apliocadaao linksMenu, mude o texto/icone para "fechar x".
+    Caso contrario, mostre o texto/icone "Menu &equiv;"   */
+    if (linksMenu.classList.contains("aberto")) {
+        icone.innerHTML = "Fechar &times";
+    } else {
+        icone.innerHTML + "Menu &equiv";
+    }
+
+});
